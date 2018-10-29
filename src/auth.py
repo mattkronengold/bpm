874990509759be3e02922c542df783d700a08bc5
alana anderson <afa2132@columbia.edu>
2018-10-28 20:38:16 -0400

    changes to auth

--- src/auth.py
+++ src/auth.py
@@ -1,10 +1,13 @@
+import pickle
+import requests
 import sys
+import time
 import webbrowser
 
 
 CLIENT_ID = '482102fb45cb45fdb465ef73801f4665'
 CLIENT_SECRET = 'dc7269e0e5a84e71b9b27f857055b41f'
-REDIRECT_URI = 'https://localhost/'
+REDIRECT_URI = 'https://localhost:5233/capture'
 SCOPES = 'user-library-modify user-read-playback-state user-read-currently-playing user-modify-playback-state user-read-recently-played'
 
 
@@ -18,3 +21,29 @@ def authenticate():
 	webbrowser.open(auth_req)
 
 	#finish login process
+
+def finish_auth(auth_code):
+    # application requests access tokens and refresh tokens
+    resp = requests.post("https://accounts.spotify.com/api/token",
+                         data={"grant_type": "authorization_code",
+                               "redirect_uri": REDIRECT_URI,
+                               "code": auth_code},
+                         auth=(CLIENT_ID, CLIENT_SECRET))
+    resp.raise_for_status()
+
+    # pickle tokens so user does not have to re-authenticate
+    resp_json = resp.json()
+    pickle_data = {
+        "access_token": resp_json["access_token"],
+        "refresh_token": resp_json["refresh_token"],
+        "expires_in": resp_json["expires_in"],
+        "last_refreshed": time.time(),
+    }
+
+    # DO NOT DUMP TO PICKLE
+    with open("token.pk", "wb") as pickle_file:
+        pickle.dump(pickle_data, pickle_file)
+
+    sqlalchemy.whatever.exec('INSERT INTO user_auth (access_token, refresh_token, expires_in, last_refreshed) VALUES (%s, %s, %s, %s)', access_token, refresh_token, expires_in, last_refreshed)
+
+
