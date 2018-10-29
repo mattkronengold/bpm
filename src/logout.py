a6d9fcff57869b79d00bf390e1d33ce019a7294e
Katie Pfleger <kjp2157@columbia.edu>
2018-10-29 13:44:05 -0400

    logout function now working

--- src/logout.py
+++ src/logout.py
@@ -6,18 +6,12 @@ c = conn.cursor()
 
 def logout():
     # get current user
-    username = c.execute('SELECT username FROM User U JOIN Credentials C ON U.id = C.user_id;') # join on id
-    user_id = c.execute('SELECT user_id FROM User U JOIN Credentials C ON U.id = C.user_id;') # get user_id of join
-    username = username.fetchone()[0]
-    user_id = user_id.fetchone()[0]
-    print(username)
+    user_id = c.execute('SELECT C.user_id FROM Credentials C').fetchone()[0] # get user_id of join
     print(user_id)
 
     # listen for logout
     if (input() == 'logout'):
-        c.execute('DELETE FROM Credentials C WHERE C.user_id = ?;', (user_id,))
+        c.execute('DELETE FROM Credentials WHERE user_id = ?;', (user_id,))
         conn.commit()
-        # delete access_token and refresh_token from database
-        # terminate program
 
 logout()
