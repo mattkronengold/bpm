c45f6815de1d98ce020796520ddafe3b3bf05161
alana anderson <afa2132@columbia.edu>
2018-10-28 20:58:13 -0400

    more auth

--- src/auth.py
+++ src/auth.py
@@ -4,12 +4,10 @@ import sys
 import time
 import webbrowser
 
-
 CLIENT_ID = '482102fb45cb45fdb465ef73801f4665'
 CLIENT_SECRET = 'dc7269e0e5a84e71b9b27f857055b41f'
-REDIRECT_URI = 'https://localhost:5233/capture'
+REDIRECT_URI = 'https://localhost/'
 SCOPES = 'user-library-modify user-read-playback-state user-read-currently-playing user-modify-playback-state user-read-recently-played'
-
 
 def authenticate():
 	auth_req = "https://accounts.spotify.com/authorize" + \
@@ -19,8 +17,12 @@ def authenticate():
 	"&response_type=code" 
 	auth_req.replace(":", "%3A").replace("/", "%2F").replace(" ", "+")
 	webbrowser.open(auth_req)
-
 	#finish login process
+
+def get_code():
+    print("Please copy and paste your validation code from the browser: ")
+    auth_code = input()
+    finish_auth(auth_code)
 
 def finish_auth(auth_code):
     # application requests access tokens and refresh tokens
@@ -44,6 +46,7 @@ def finish_auth(auth_code):
     with open("token.pk", "wb") as pickle_file:
         pickle.dump(pickle_data, pickle_file)
 
-    sqlalchemy.whatever.exec('INSERT INTO user_auth (access_token, refresh_token, expires_in, last_refreshed) VALUES (%s, %s, %s, %s)', access_token, refresh_token, expires_in, last_refreshed)
+    # sqlalchemy.whatever.exec('INSERT INTO user_auth (access_token, refresh_token, expires_in, last_refreshed) VALUES (%s, %s, %s, %s)', access_token, refresh_token, expires_in, last_refreshed)
 
-
+authenticate() 
+get_code()
