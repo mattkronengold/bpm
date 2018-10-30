50c5885736af3f04bdfd36c4232de9ea0c2847ae
Matt Kronengold <mak2265@columbia.edu>
2018-10-29 22:56:16 -0400

    Update pre-commit hook

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 
-pylint src
+# pre-commit.sh
+git stash -q --keep-index
+
+pylint src --rcfile=pylint_settings.rc
+RESULT1=$?
+
 pytest
+RESULT2=$?
+
+git stash pop -q
+
+if [ $RESULT1 -ne 0 ] || [ $RESULT2 -ne 0 ]
+then
+	echo "Pre-commit hook failed"
+	exit 1
+fi
+
+exit 0
