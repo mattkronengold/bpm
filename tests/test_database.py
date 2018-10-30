24b72c6db8723a9ea3fbe654e334c8d66d17cd6e
Tristan Orlofski <tio2001@columbia.edu>
2018-10-30 15:26:09 -0400

    Added databases test file

---
+++ tests/test_database.py
@@ -0,0 +1,33 @@
+'''
+
+@author: torlofski
+
+'''
+import sqlite3
+from sqlite3 import Error
+from src.database import create_connection, create_user, create_credentials
+import os.path
+
+def test_databases():
+    db_file = "bpm_t.db"
+
+    try:
+        create_connection(db_file)
+        assert(os.path.isfile(db_file))
+
+        create_user(db_file)
+        conn = sqlite3.connect(db_file)
+        c = conn.cursor()
+        user_table_exists = c.execute("SELECT count(*) from sqlite_master WHERE type='table' AND name='user'").fetchone()
+        assert(user_table_exists)
+
+        create_credentials(db_file)
+        creds_table_exists = c.execute("SELECT count(*) from sqlite_master WHERE type='table' AND name='credentials'").fetchone()
+        assert(creds_table_exists)
+    except Error as e:
+        print(e)
+    finally: 
+        conn.close()
+
+    
+    
