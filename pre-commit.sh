87523cf72fab8f2f199ba97b6a6dd289aeadedd9
Matt Kronengold <mak2265@columbia.edu>
2018-10-30 15:12:50 -0400

    Update pre-commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
 # pre-commit.sh
-git stash -q --keep-index
-
 pylint src --rcfile=pylint_settings.rc
 RESULT1=$?
 
 pytest
 RESULT2=$?
-
-git stash pop -q
 
 if [ $RESULT1 -ne 0 ] || [ $RESULT2 -ne 0 ]
 then
