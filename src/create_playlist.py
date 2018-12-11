8aad561dbc91e568eea2b79d46a3b10c39d146eb
alana anderson <afa2132@columbia.edu>
2018-12-11 11:12:28 -0500

    open spotify

--- src/create_playlist.py
+++ src/create_playlist.py
@@ -1,10 +1,11 @@
 '''
 
-@author: torlofski
+@author: torlofskii, alanaanderson
 
 '''
 from __future__ import print_function
 import spotipy
+import webbrowser
 
 def create_playlist(token, tracks):
     """Creates playlist from user information and generated tracks"""
@@ -16,11 +17,19 @@ def create_playlist(token, tracks):
     for track in tracks:
         final_song_list.append(track["tid"])
 
-    name = input('Enter a name for your new playlist!\n')
+    name = input('Enter a name for your new playlist:\n')
+    print()
 
     playlist = spotify.user_playlist_create(user_id, name)
     playlist_id = playlist['id']
 
     for song in final_song_list:
         spotify.user_playlist_add_tracks(user_id, playlist_id=playlist_id, tracks=[song])
-    print("Added tracks to playlist!")
+    print("Would you like to open Spotify now?")
+    print('0:\tYes')
+    print('1:\tNo')
+    open_spotify = input()
+    print()
+    if open_spotify == '0':
+        webbrowser.open('http://open.spotify.com/playlist/'+ playlist_id)
+    print('Your playlist has been saved to Spotify!')
