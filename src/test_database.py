2de08ff8ffb7fabed10d4d6ee0a53675e14e4510
Matt Kronengold <mak2265@columbia.edu>
2018-12-12 10:54:36 -0500

    Update database tests

--- src/test_database.py
+++ src/test_database.py
@@ -6,7 +6,7 @@
 import sqlite3
 from sqlite3 import Error
 import os.path
-from src.database import create_connection, create_user, create_credentials
+from src.database import create_connection, verify_tables
 
 
 def test_databases():
@@ -18,20 +18,29 @@ def test_databases():
         create_connection(db_file)
         assert os.path.isfile(db_file)
 
-        create_user(db_file)
+        verify_tables(db_file)
+
         conn = sqlite3.connect(db_file)
         c = conn.cursor()
+
         user_table_exists = c.execute("SELECT count(*) from \
-            sqlite_master WHERE type='table' AND name='user'").fetchone()
+            sqlite_master WHERE type='table' AND name='User'").fetchone()
         assert user_table_exists
 
-        create_credentials(db_file)
         creds_table_exists = c.execute("SELECT count(*) from \
-            sqlite_master WHERE type='table' AND name='credentials'").fetchone()
+            sqlite_master WHERE type='table' AND name='Credentials'").fetchone()
         assert creds_table_exists
 
-        os.remove('bpm_t.db')
+        playlist_table_exists = c.execute("SELECT count(*) from \
+            sqlite_master WHERE type='table' AND name='Playlist'").fetchone()
+        assert creds_table_exists
+
+        dislikes_table_exists = c.execute("SELECT count(*) from \
+            sqlite_master WHERE type='table' AND name='Dislikes'").fetchone()
+        assert dislikes_table_exists
+
     except Error as e:
         print(e)
     finally:
         conn.close()
+        os.remove(db_file)
